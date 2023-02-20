02b1fb83961bac2e5e5c2dae13bc68b2bcab854d
Shivam Tripathi <57054054+Shivam89539@users.noreply.github.com>
2023-02-20 22:15:59 +0000

    home page

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
 @app.route('/')
-def helloWorld():
-  return "hello,world!"
+def shivam():
+  return render_template('home.html')
+
 
 if __name__ == '__main__ ':
-  app.run(host='0.0.0.0',debug=True)
-  
+  app.run(host='0.0.0.0', debug=True)
